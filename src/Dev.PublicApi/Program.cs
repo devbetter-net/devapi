6be87f9406e371211c7a4e9dc3fead641a48897e
kyle <chitmeo@chitmeo.com>
2024-03-17 13:30:30 +0700

    Add new files and configurations for Dev.Tests and Dev.PublicApi

---
+++ src/Dev.PublicApi/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+const string CORS_POLICY = "CorsPolicy";
+var corsOrigins = builder.Configuration.GetSection("CorsWithOrigins").Get<string[]>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: CORS_POLICY,
+        corsPolicyBuilder =>
+        {
+            corsPolicyBuilder.WithOrigins(corsOrigins ?? Array.Empty<string>());
+            corsPolicyBuilder.AllowAnyMethod();
+            corsPolicyBuilder.AllowAnyHeader();
+        });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors(CORS_POLICY);
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.Run();
